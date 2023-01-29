19e73a22d3ff88d61c8a87125ad32282b55b84ff
kabirshuvo <kabirshuvo19@gmail.com>
2023-01-30 01:37:42 +0600

    get property valus in 3 ways

--- modules/module_4/objects.js
+++ modules/module_4/objects.js
@@ -1,21 +1,46 @@
-const student = {
-    id: 115,
-    name: 'Xadu.life',
-    class: 10,
-    marks: 99
-}
+//...........................
+// <<<:::OBJECTS:::>>>
 
-const mobile = {
-    brand: 'Nokia',
-    price: 19000,
-    storage: 32,
-    camera: 7,
-}
+// const student = {
+//     id: 115,
+//     name: 'Xadu.life',
+//     class: 10,
+//     marks: 99
+// }
 
-const myComputer = {
-brand: 'Intel',
-price: 35000,
-color: 'Black',
-processor: 'i5',
+// const mobile = {
+//     brand: 'Nokia',
+//     price: 19000,
+//     storage: 32,
+//     camera: 7,
+// }
+
+// const myComputer = {
+// brand: 'Intel',
+// price: 35000,
+// color: 'Black',
+// processor: 'i5',
+// }
+// console.log(myComputer);
+const shopingCart = {
+    biri: '3pack',
+    rice: '25kg',
+    daal: '5kg',
+    icecream: 'zoomanzee',
+    tamak: 25,
 }
-console.log(myComputer);
+//console.log(shopingCart.biri);
+//......................................................
+// ...Multiple Ways to get and set object keys value...
+//......................................................
+
+let riceCount = shopingCart.rice;
+//console.log(riceCount);
+
+// let properties = Object.keys(shopingCart);
+// console.log(properties);
+// let propertyValues = Object.values(shopingCart);
+// console.log(propertyValues);
+let propertyName = 'biri';
+let propertyValue = shopingCart[propertyName];
+console.log(propertyName, propertyValue);
