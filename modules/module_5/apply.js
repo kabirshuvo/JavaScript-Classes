3d8efa6234989a02375bd3522ab8779b5a5d7a97
kabirshuvo <kabirshuvo19@gmail.com>
2023-01-31 00:22:39 +0600

    Mooving Forward

---
+++ modules/module_5/apply.js
@@ -0,0 +1,33 @@
+// let myInches = 12;
+// let myFeet = myInches / 12;
+// console.log('My Choice:', myFeet);
+
+// let dadaInches = 144;
+// let dadaFeet = dadaInches / 12;
+// console.log('Dadar Choice:', dadaFeet);
+
+// function inchToFeet(inches){
+//     const feet = inches /12;
+//     return feet;
+// }
+
+// let nanaInch = 100;
+// let nanaFeet = inchToFeet(nanaInch);
+// console.log(nanaFeet);
+
+// let dadaInches = 144;
+// let dadaFeet = inchToFeet(dadaInches);
+// console.log(dadaFeet);
+
+//<<<<miles to kilomiter>>>>
+function milesToKilo(miles){
+    const kilomiter = miles*1.61;
+    return kilomiter;
+}
+let myMiles = 10;
+let myKilos = milesToKilo(myMiles);
+console.log(myKilos);
+
+let zooMiles = 20;
+let zooKilo = milesToKilo(zooMiles);
+console.log(zooKilo);
