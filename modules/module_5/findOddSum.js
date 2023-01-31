7f5489d0d61f31f8275266feadf7ffa1621190ce
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-01 04:24:35 +0600

    Its revision time

--- modules/module_5/findOddSum.js
+++ modules/module_5/findOddSum.js
@@ -1,31 +1,38 @@
 
-function getSum(numbers){
-   let sum = 0;
-   for(let i =0; i < numbers.length; i++){
-    const index = i;
-    const element = numbers[index];
-    sum = sum + element;
-    //console.log(sum);
-   }
-   return sum;
-}
+// function getSum(numbers){
+//    let sum = 0;
+//    for(let i =0; i < numbers.length; i++){
+//     const index = i;
+//     const element = numbers[index];
+//     sum = sum + element;
+//     //console.log(sum);
+//    }
+//    return sum;
+// }
 
-function getOdds(numbers){
-    let oddNumbers =[];
-    for(let i = 0; i < numbers.length; i++){
-        const index = i;
-        const element = numbers[index];
-        if(element % 2 == 0){
-            oddNumbers.push(element);
-            //console.log(oddNumbers);
-        }
-    }
-    return oddNumbers;
-}
+// function getOdds(numbers){
+//     let oddNumbers =[];
+//     for(let i = 0; i < numbers.length; i++){
+//         const index = i;
+//         const element = numbers[index];
+//         if(element % 2 !== 0){
+//             oddNumbers.push(element);
+//             console.log(oddNumbers);
+//         }
+//     }
+//     return oddNumbers;
+// }
 
-const myNumbers = [12, 65, 45, 78, 32, 85, 91];
-const total = getSum(myNumbers);
-const oddNumbers = getOdds(myNumbers);
-const oddSum = getSum(oddNumbers);
+// const myNumbers = [12, 65, 45, 78, 32, 85, 91];
+// const total = getSum(myNumbers);
+// const oddNumbers = getOdds(myNumbers);
+// const oddSum = getSum(oddNumbers);
 
-console.log(total);
+// console.log(total);
+
+function print(a, b, c){
+    return a+2;
+    return a*b;
+    return b*c+a;
+  }
+  console.log(print(1,2,3));
