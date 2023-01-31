fccbe5675129649ce2e936a986f1ae97b2e54cf9
kabirshuvo <kabirshuvo19@gmail.com>
2023-01-31 15:19:04 +0600

    Got the OddSum

--- modules/module_5/findOddSum.js
+++ modules/module_5/findOddSum.js
@@ -1,5 +1,3 @@
-const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getSum(myNumbers);
 
 function getSum(numbers){
    let sum = 0;
@@ -7,7 +5,26 @@ function getSum(numbers){
     const index = i;
     const element = numbers[index];
     sum = sum + element;
-    console.log(index, element, sum);
+    //console.log(sum);
    }
    return sum;
 }
+
+function getOdds(numbers){
+    let oddNumbers =[];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 0){
+            oddNumbers.push(element);
+            //console.log(oddNumbers);
+        }
+    }
+    return oddNumbers;
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 85, 91];
+const oddNumbers = getOdds(myNumbers);
+const oddSum = getSum(oddNumbers);
+
+console.log(oddSum);
