0da410e4a2279e5e792f457e6672a5220b98be5c
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-02 01:49:48 +0600

    Max problem solved

--- modules/module_6/max.js
+++ modules/module_6/max.js
@@ -1,1 +1,17 @@
-console.log(999)
+
+function maxInArray(numbers){
+    let largest = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element > largest){
+            largest = element;
+        }
+    }
+    return largest;
+}
+
+
+const heights = [167, 190, 120, 165, 137];
+const tallest = maxInArray(heights);
+console.log('tallest person is:', tallest);
