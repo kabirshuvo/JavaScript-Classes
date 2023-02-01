ac77e75937466ce8d41be5d7aa4fd37d2f3246d1
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-01 23:24:57 +0600

    Math Have to be practice more

---
+++ modules/module_6/math.js
@@ -0,0 +1,18 @@
+const result = Math.pow(4, 4);
+// * console.log(result);
+
+// const num1 = 25;
+// const num2 = 45;
+
+// const gap = Math.abs(num1 - num2);
+
+// if(gap < 5){
+//     console.log('You are Good');
+// }
+
+// else{
+//     console.log('stay Apart')
+// }
+
+// * Search Math MDN and read every day
+
