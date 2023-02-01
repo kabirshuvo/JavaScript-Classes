d8fea0ae6247be1d9616310bd8d599adcbfc702c
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-01 23:03:31 +0600

    Kala Kala Sada Sada

--- modules/module_6/problemSolving.js
+++ modules/module_6/problemSolving.js
@@ -5,20 +5,37 @@
 // const addressLength = myAddress.length;
 // console.log(addressLength);
 
-const lyrics = 'Tumi Bondhu Kala Pakhi, ami jeno ki. Boshonto kale tomai koite parini';
+//const lyrics = 'Tumi Bondhu Kala Pakhi, ami jeno ki. Boshonto kale tomai bolte parini';
 
-const searchFor = 'Kala Pakhi'
-const doseExist = lyrics.toLowerCase().includes(searchFor.toLowerCase());
-console.log(doseExist);
+// const searchFor = 'Kala Pakhi'
+// const doseExist = lyrics.toLowerCase().includes(searchFor.toLowerCase());
+// console.log(doseExist);
 
-if(lyrics.indexOf('Bondhu') !== -1){
-console.log('This word Exist in the lyrics');
-}
+// if(lyrics.indexOf('Bondhu') !== -1){
+// console.log('This word Exist in the lyrics');
+// }
 
-else{
-    console.log('Nai Nai Nai re Pagol')
-}
+// else{
+//     console.log('Nai Nai Nai re Pagol')
+// }
 
-// *** starts With
+//  *** starts With
 
-// *** endswith
+//  *** ends with
+
+// *** How to split, slice, substr, substring, concat, join
+
+// const lyrics = 'Tumi Bondhu. Kala Pakhi. ami jeno ki. Boshonto kale tomai. bolte parini';
+// const parts = lyrics.split(' ');
+// const sentence = lyrics.split('.');
+// const chars = lyrics.split('');
+
+
+// ***console.log(chars);
+
+// const lines = [
+//     'Tumi Bondhu.', 'Kala Pakhi.', 'ami jeno ki.', 'Boshonto kale tomai.', 'bolte parini.'
+// ];
+
+// const newLyric = lines.join('-');
+// console.log(newLyric);
