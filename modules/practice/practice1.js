97968f6946d956060c42120a9b2b0389ed42d90c
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-04 13:31:49 +0600

    practice 1 started

---
+++ modules/practice/practice1.js
@@ -0,0 +1,8 @@
+const country = 'Argentina';
+const age = 52;
+const isIndependent = true;
+const student = {id: 121, class: 11, name: 'Agun'};
+const friends = [13, 14, 11, 17, 21, 16, 15, 20];
+
+console.log(typeof country);
+console.log(friends.length);
