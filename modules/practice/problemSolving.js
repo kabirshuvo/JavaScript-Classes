a5c51ba91d504c73430a6e8cee98083ab0472d46
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-05 18:14:25 +0600

    feetToinch done in problemSolving.js

--- modules/practice/problemSolving.js
+++ modules/practice/problemSolving.js
@@ -1,20 +1,31 @@
-let myArray = [
-    45,
-    87,
-    -99,
-    77,
-    76,
-    -34,
-    -76,
-    99
-];
-function onlyPositive(numbers){
-    let positive = [];
-    for (let i = 0; i < numbers.length; i++){
-        if (numbers[i] > 0){
-            positive.push(numbers[i]);
-        }
-    }
-    return positive;
-}
-console.log(onlyPositive(myArray));
+// *** get only positive numbers from an array
+
+// let myArray = [
+//     45,
+//     87,
+//     -99,
+//     77,
+//     76,
+//     -34,
+//     -76,
+//     99
+// ];
+// function onlyPositive(numbers){
+//     let positive = [];
+//     for (let i = 0; i < numbers.length; i++){
+//         if (numbers[i] > 0){
+//             positive.push(numbers[i]);
+//         }
+//     }
+//     return positive;
+// }
+// console.log(onlyPositive(myArray));
+
+// *** inch to feet 
+
+// const feetToInch = (feet) => {
+//     return feet * 12;
+// }
+
+// const myInchs = feetToInch(10);
+// console.log('myInch =', myInchs);
