1eb2e53ce921b45414397725fa48f33187afc845
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-07 01:34:46 +0600

    Problem 3 solved

--- jsAssignment/jsAssignment.js
+++ jsAssignment/jsAssignment.js
@@ -1,31 +1,62 @@
 // ***(Problem 1: Let’s play a mind game)***
 
-function mindGame(number){
-        let inputNumber = number;
-        let newNumber = inputNumber * 3;
-        let addNumber = newNumber + 10;
-        let divideNumber = addNumber / 2;
-        let outputNumber = divideNumber - 5;
+// function mindGame(number){
+//         let inputNumber = number;
+//         let newNumber = inputNumber * 3;
+//         let addNumber = newNumber + 10;
+//         let divideNumber = addNumber / 2;
+//         let outputNumber = divideNumber - 5;
 
-        return outputNumber;
-}
+//         return outputNumber;
+// }
 
-const result1 = mindGame();
+// const result1 = mindGame();
 
+
+
+// ************************************
 // ***(Problem 2: Finding even or odd)***
+// ************************************
 
-function evenOdd(string) {
+// function evenOdd(string) {
        
-        if (string.length % 2 === 0) {
-            return "even";
-          } else {
-            return "odd";
-          }
+//         if (string.length % 2 === 0) {
+//             return "even";
+//           } 
+//           else {
+//             return "odd";
+//           }
 
+// }
+
+// const result2 = evenOdd('Batch7');
+
+
+
+// ************************************
+// ***(Problem 3: Is Less or Greater than seven)***
+// ************************************
+
+function isLGSeven(number){
+
+    let minusNumber = number -7;
+
+    if(minusNumber < 7){
+        return minusNumber;
+    }
+
+    else{
+        let doubleNumber = number * 2;
+        return doubleNumber;
+    }
 }
 
-const result2 = evenOdd("chatgpt")
-console.log(result2);
+const result3 = isLGSeven(15);
+
+
+// ************************************
+// ***(Problem 4: Finding Bad data)***
+// ************************************
 
 
 
