d3da3e163d7196266715cce8c6cfcad6fb1b1c26
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-08 11:59:34 +0600

    Basic  Module completed

---
+++ bonusJs/matching.js
@@ -0,0 +1,3 @@
+for(let i = 5; i <= 100; i++){
+    console.log(i);
+}
---
+++ bonusJs/object.js
@@ -0,0 +1,20 @@
+const student = {
+    name: 'Sakib Khan',
+    id: 121,
+    address: 'moulovi Para',
+    isSingle: true,
+    friends: ['Opu', 'Razz', 'Salam', 'Mukul'],
+    car: {
+        brand: 'tesla',
+        price: 5000000,
+        made: '2023'
+
+    }
+}
+
+console.log(student.car);
+
+
+const products = [
+    {}
+]
