024ab0c91d7218e887d1607d29df4b3ae31b6646
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-11 18:03:39 +0600

    Now can access from document

--- remoteJob/remoteJob.js
+++ remoteJob/remoteJob.js
@@ -1,7 +1,16 @@
+
 document.getElementById('div-grettings').style.backgroundColor = 'green';
 
 // document.getElementsByClassName('p-headings').style.backgroundColor = '#333';
 
 const pHeadings = document.getElementsByClassName('p-headings');
-console.log(pHeadings);
-for(const pheading of pHeadings)
+// console.log(pHeadings);
+for(const pheading of pHeadings){
+    // console.log(pheading.innerText);
+}
+const firstLi = document.querySelector('nav li');
+console.log(firstLi);
+
+const navLi = document.querySelectorAll('nav li');
+console.log(navLi);
+
