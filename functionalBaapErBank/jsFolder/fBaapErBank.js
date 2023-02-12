8b17420e9766fbbcdd12dc83fafc4bc72e5ac93b
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-13 02:44:49 +0600

    Total Deposit Section Done

--- functionalBaapErBank/jsFolder/fBaapErBank.js
+++ functionalBaapErBank/jsFolder/fBaapErBank.js
@@ -1,4 +1,38 @@
+//* Deposit-Section
+
 document.getElementById('deposit-btn').addEventListener('click', function(){
-console.log('clicked');
+    const dipositField = document.getElementById('new-deposit');
+    const newDipositAmmountString = dipositField.value;
+    //Setting the initial value an empty string;
+    dipositField.value ='';
+    const  newDipositAmmount = parseFloat(newDipositAmmountString);
+    // I got the new deposit ammount here
+
+    const previousDepositElement = document.getElementById('deposit-total');
+    const previousDepositAmmountString = previousDepositElement.innerText;
+    const previousDepositAmmount = parseFloat(previousDepositAmmountString);
+    // Got the previous Ammount
+ 
+    const inTotalDeposit = previousDepositAmmount + newDipositAmmount;
+    previousDepositElement.innerText = inTotalDeposit;
+    
+    console.log(inTotalDeposit);
 });
 
+
+
+
+
+
+//* withdraw-Section
+
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const withdrawField = document.getElementById('new-withdraw');
+    const newWithdrawAmmountString = withdrawField.value;
+    const newWithdrawAmmount = parseFloat(newWithdrawAmmountString);
+    withdrawField.value = '';
+    // I got the new withdraw Ammount;
+
+
+
+});
