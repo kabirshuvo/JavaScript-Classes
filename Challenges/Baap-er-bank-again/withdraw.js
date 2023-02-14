b38feb4d05ce437d84008400e04d2e99b63e2e69
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-14 16:03:20 +0600

    Challange 1 done, But I have to see the function setElement Value

--- Challenges/Baap-er-bank-again/withdraw.js
+++ Challenges/Baap-er-bank-again/withdraw.js
@@ -1,3 +1,19 @@
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-console.log('button Clicked');
+const withdraw = getValueFromInpuField('new-withdraw');
+const previousWithdraw = getValueFromTextElement('withdraw-total');
+const totalWithdraw = previousWithdraw + withdraw;
+const balance = getValueFromTextElement('Balance-total');
+const currentBalance = balance - withdraw;
+
+if(isNaN(withdraw)){
+    alert('Please Input a Valid Number');
+    return;
+}
+if(withdraw > currentBalance){
+    alert('not Enough Fund');
+    return;
+}
+setValueTextElement('withdraw-total', totalWithdraw);
+
+setValueTextElement('Balance-total', currentBalance);
 });
