e7ce920af0291e46b314c2eddb928ea756d0598c
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-17 17:44:18 +0600

    Going good

---
+++ Challenges/Discount-1/discount.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('discount-btn').addEventListener('click', function(){
+    
+
+    const totalPrice = 
+    
+    const discountInput = document.getElementById('discount');
+    const discountString = discountInput.value; 
+    const discountValue = parseInt(discountString);
+
+
+
+
+    console.log(discountValue);
+})
