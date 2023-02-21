c6ec2f0cf00bc3d8ca047a1b4361090f9362b422
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-21 14:21:59 +0600

    Started es6 with summit

---
+++ ES6/es6.js
@@ -0,0 +1,18 @@
+// Fat Arrow function
+
+//* version 1
+// function number(){
+//     return 10;
+// }
+
+//* version 2
+let number = () => {
+    return 10;
+}
+
+//* version 3 (don't have to write return)
+
+let number1 = () => 10;
+
+
+console.log(number1());
---
+++ modules/esSixAdvance/filter.js
@@ -0,0 +1,9 @@
+const product = {
+    name: 'Laptop', 
+    model:'Yoga 3', 
+    price:49000, dusk: '512SSD'
+}
+
+const {price} = product;
+
+console.log(price);
