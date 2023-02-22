e91c3355acef3aed0aaed893b714a256440829c4
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-22 23:55:17 +0600

    Understanding fetch API

--- modules/exploreAPI/fetch.js
+++ modules/exploreAPI/fetch.js
@@ -1,5 +1,15 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
 
-      
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// fetch(url)
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+function loadData(){
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
