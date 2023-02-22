ee1efee79f88d845cc0c0c59fa928b9dc9fa6f37
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-22 22:29:26 +0600

    Started Jeson

--- modules/esSixAdvance/map.js
+++ modules/esSixAdvance/map.js
@@ -2,4 +2,44 @@ const numbers = [2, 8, 4, 6, 3];
 
 const output = [];
 
-//for(const number of )
+// for(const number of numbers){
+//     const single = number;
+//     output.push(single)
+//     const double = number * 2 ;
+//     output.push(double);
+//     const tripple = number * 3;
+//     output.push(tripple);
+// }
+
+//console.log(output); 
+const doubleIt = num => num *2;
+function getDoubles(numbers){
+    const output = [];
+    for (const number of numbers){
+        const doubled = doubleIt(number);
+        output.push(doubled);
+    }
+    return output;
+} 
+const result = getDoubles(numbers)
+
+
+console.log(result);
+
+
+
+/* *
+ * purpose: 
+1. get an array
+2. for every elements of the array do something
+3. store the result in an array
+4. return the result array
+ */
+
+
+const makeDubDabol = numbers.map(doubleIt)
+
+//console.log(makeDubDabol);
+const makeDouble = numbers.map(x => x * 2);
+
+//console.log(makeDouble);
---
+++ modules/exploreAPI/exploreAPI.js
@@ -0,0 +1,30 @@
+const user = {
+    id: 1, name: 'KaaBir', job: 'tobbing'
+}; 
+
+const stringified = JSON.stringify(user)
+
+
+// console.log(user);
+
+// console.log(stringified);
+
+const shop = {
+    owner: 'ZOO',
+    address: {
+            thana: 'raipur',
+            tana: 'mollarHaat',
+            hana: 'kerowa', country: 'Bangladesh'
+    },
+
+    product: ['lapto', 'mic', 'monitor', 'keyboard'],
+    revenue: 500,
+    isOpen: true, isOld: false
+};
+const shopJason = JSON.stringify(shop);
+console.log(shopJason);
+
+
+const shopObj = JSON.parse(shopJason);
+console.log(shopObj);
+
