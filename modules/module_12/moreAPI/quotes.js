341ab35a04c3249ea769369a30aad2ae2edf118f
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-23 21:09:31 +0600

    Kanye West Quotes are displaying correctly

---
+++ modules/module_12/moreAPI/quotes.js
@@ -0,0 +1,17 @@
+const loadQuote = () =>{
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuote(data))
+}
+
+const displayQuote = quote => {
+   const blockQuote = document.getElementById('quote');
+   console.log(quote);
+
+   blockQuote.innerHTML = quote.quote;
+
+}
+
+
+
+loadQuote();
