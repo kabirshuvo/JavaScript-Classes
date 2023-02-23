c938df83d5946936e16cbb2954f4359906f0dad2
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-23 19:15:24 +0600

    Api tp todo Display done

---
+++ modules/exploreAPI/todos.js
@@ -0,0 +1,26 @@
+function loadTodos(){
+   fetch('https://jsonplaceholder.typicode.com/todos')
+   .then(res => res.json())
+   .then(data => displayTodos(data))
+}
+
+function displayTodos(todos){
+    //*get the container
+    const todoContainer = document.getElementById('todo-container');
+
+    for(const todo of todos){
+        //*Create the child element
+        const todoDiv = document.createElement('div');
+        //*set innerText or innerHTML
+        todoDiv.innerHTML =`
+        <h3>title: ${todo.title}</h3>
+        <p>User: ${todo.userId}</p>
+
+        `;
+        //*appendChild
+        todoContainer.appendChild(todoDiv);
+    }
+
+}
+
+loadTodos();
