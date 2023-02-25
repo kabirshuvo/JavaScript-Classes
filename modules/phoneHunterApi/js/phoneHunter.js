89c34b0656fe04bfb5c32249f4d33242b8e3ca4a
kabirshuvo <kabirshuvo19@gmail.com>
2023-02-26 01:06:09 +0600

    Phones and phone details are displaying well and good

--- modules/phoneHunterApi/js/phoneHunter.js
+++ modules/phoneHunterApi/js/phoneHunter.js
@@ -11,7 +11,24 @@ const loadPhones = async() => {
 const displayPhone = phones =>{
     const phonesContainer = document.getElementById('phone-container');
     phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML =`
+        <div class="card p-4">
+        <img src="${phone.image}" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">
+            ${phone.slug}
+          </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
         
+        `;
+        phonesContainer.appendChild(phoneDiv);
     })
 }
 
