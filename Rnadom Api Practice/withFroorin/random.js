c1facb6c9d9bde8961c6c55df3407c974a7be467
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-01 23:52:36 +0600

    Getting Lost

--- Rnadom Api Practice/withFroorin/random.js
+++ Rnadom Api Practice/withFroorin/random.js
@@ -1,45 +1,21 @@
 const getDogs = async () => {
     //Random Dog Api
-    const URL = `{"fileSizeBytes":3356271,"url":"https://random.dog/5d433414-7e79-4d14-9951-640cb7a85c3f.mp4"}`
+    const URL = `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=Danny%20Welbeck`
 
     try{
         console.log('load Start')
 
         const res = await fetch(URL);
         const data = await res.json();
-
-        console.log(data.slice(0, 10));
-
+        displayMe(data.player);
         console.log('load last')
     } catch (error){
         console.log(error);
     }
 }
 
-const displayDogs = (dogs) => {
-
-    console.log(dogs);
-
-    const dogsContainer = document.getElementById('dogs-container').addEventListener('click', function(){
-        dogs.forEach(dog =>{
-            
-        console.log(dog); 
-
-            const imgUrl = dogs.imgUrl ? dogs.imageurl: 'No img found'
-
-        const displayDiv = document.createElement('div');
-        displayDiv.classList.add('')
-        displayDiv.innerHTML = `
-        <img src="${imgUrl}">
-        `;
-        dogsContainer.appendChild(displayDiv);
-        
-        })
-
-        
-    });
-    
-
+const displayMe = () =>{
+   console.log(data.player)
 }
 
 getDogs();
