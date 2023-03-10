f2151ed3942c9eae903dcc0da11029c93efbecf0
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-11 05:27:12 +0600

    clickCount is working on Local storage

--- localStpragePractice/clickCount.js
+++ localStpragePractice/clickCount.js
@@ -1,20 +1,13 @@
-// const clickCount = () => {
-//     let count = 0;
-//     count ++; 
-//     document.getElementById("clickCount").innerText = count;
-//     }
-
-
-
-
 let count = 0;
+let storedCount = localStorage.getItem('count');
+if(storedCount){
+    count = parseInt(storedCount);
+}
 document.getElementById('clickCount-btn').addEventListener('click', function (){
     console.log('btn-clicked');
     
     count+=1 ; 
     document.getElementById("clickCount").innerText = count;
-
+    localStorage.setItem('count:', count);
     
-} )
-
-const 
+} );
