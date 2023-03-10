be1a43221678f19a401c298dbc5797136065a8f1
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-11 02:37:04 +0600

    Object serialized

--- localStpragePractice/local.js
+++ localStpragePractice/local.js
@@ -1,6 +1,36 @@
 console.log('Keep Practicing');
 
 
+
+const reload =()=>{
+    window.location.reload(true);
+}
+
+const getAge = localStorage.setItem('age', '40');
+
+//const age = localStorage.age = ('age', '40');
+
+
+const getItem = () =>{
+    const age = localStorage.getItem('age');
+    document.getElementById('showItemUl').innerHTML = age;
+};
+
+const removeItem = () =>{
+    localStorage.removeItem('age');
+}
+
 const clearItem = () => {
     localStorage.clear()
 }
+
+const name = {firseName: 'Abraham', lastName:'Linkon'}
+
+const personStringify = JSON.stringify(name);
+
+//const atLocal = localStorage.setItem(personStringify);
+localStorage.setItem('name',name);
+
+// const setItem = () => {
+//     localStorage.setItem('atLocal')
+// }
