147941a718e94639183a336231c30a5f664f52c3
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-14 21:51:38 +0600

    Array destructuring Understanding

--- revision/destructuring.js
+++ revision/destructuring.js
@@ -1,3 +1,25 @@
-const alphabet  = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-const nubbers   = ['1', '2', '3', '4', '5', '6', '7'];
+const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+const numbers   = ['1', '2', '3', '4', '5', '6', '7'];
+
+// const a = alphabets[0];
+// console.log(a);
+
+// const b = alphabets[1];
+// console.log(b);
+
+const [a,, c, ...rest] = alphabets;
+
+const newArray = [...alphabets, ...numbers]
+
+const boxify = alphabets.concat(numbers);
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+//console.log(rest)
+
+
+console.log(newArray);
+console.log(boxify);
 
