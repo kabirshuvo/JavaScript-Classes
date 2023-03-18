20ac7dc3742ab26e8ab97f2b5653f0323212e8e0
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-18 20:55:03 +0600

    Starting Revision Again

---
+++ revision/objectDestructuring.js
@@ -0,0 +1,34 @@
+const zooPinu = {
+    name: 'Azaira',
+    age: '32',
+    personalty: {
+        activity: 'brest Feeding',
+        child: 1,
+        smiled: 'rearly'
+    },
+    address: {
+        city: 'Noakhali',
+        state: 'Chitagong'
+    }
+
+}
+
+const meInfo = {
+    name: 'kabir',
+    fame: 'NAN',
+    age: 40,
+    personality: {
+        activity: 'timeCount',
+        child: 2, 
+        smiled: 'veryFew'
+    },
+    address: {
+        city: 'Natore',
+        state: 'Rajshahi'
+    }
+}
+//const {name, age} = zooPinu;
+const {name, age} = meInfo;
+
+console.log(name);
+console.log(age);
